9e01a9ecd472118399d28453ed2a0e38206b0f6b
RahulGupta2285 <contact.rahul2285@gmail.com>
2023-11-26 13:06:51 +0530

    changes

--- bubble.java
+++ bubble.java
@@ -1,24 +1,31 @@
 
 public class bubble {
 
-    public static void sort(int arr[]) {
+    public static int sort(int arr[]) {
+        int swaps = 0;
         for (int turn = 0; turn < arr.length - 1; turn++) {
             for (int j = 0; j < arr.length - 1 - turn; j++) {
                 if (arr[j] > arr[j + 1]) {
                     // swap
+                    swaps++;
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
             }
         }
+        return swaps;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 5, 4, 1, 3, 2 };
-        sort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        int arr[] = { 5, 3, 2, 1 };
+        if (sort(arr) != 0) {
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        } else {
+            System.out.println("array is sorted");
         }
+
     }
 }
