7b7a2c43deaa64ed19b8812d0efeceb1e399b011
RahulGupta2285 <contact.rahul2285@gmail.com>
2023-11-26 12:24:06 +0530

    changes

--- bubble.java
+++ bubble.java
@@ -1,31 +1,24 @@
+
 public class bubble {
 
-    // Time complexity of this program is O(n2)
-
-    public static void swap(int arr[], int a, int b) {
-        arr[a] += arr[b];
-        arr[b] = arr[a] - arr[b];
-        arr[a] -= arr[b];
-    }
-
-    // bubble sort
-    public static void bubbleSort(int arr[]) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
+    public static void sort(int arr[]) {
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
                 if (arr[j] > arr[j + 1]) {
                     // swap
-                    swap(arr, j, j + 1);
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
     }
 
     public static void main(String[] args) {
-        int arr[] = { 5, 4, 3, 2, 1 };
-        bubbleSort(arr);
-        for (int item : arr) {
-            System.out.print(item + " ");
+        int arr[] = { 5, 4, 1, 3, 2 };
+        sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
