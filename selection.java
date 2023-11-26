d4389f57f3a6878b9d06df2e334c8451d098e3fa
RahulGupta2285 <contact.rahul2285@gmail.com>
2023-11-26 13:52:13 +0530

    changes

--- selection.java
+++ selection.java
@@ -1,33 +1,27 @@
+
 public class selection {
 
-    // Time complexity of this program is O(n2)
-
-    public static void swap(int arr[], int a, int b) {
-        int temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
-    }
-
-    public static void selectionSort(int arr[]) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            int smallest = i;
-            // finding the minimum element
-            for (int j = i + 1; j < n; j++) {
-                if (arr[smallest] > arr[j]) {
-                    smallest = j;
+    public static void sort(int arr[]) {
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            int min = turn;
+            for (int j = turn + 1; j < arr.length; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
                 }
             }
             // swap
-            swap(arr, i, smallest);
+            int temp = arr[min];
+            arr[min] = arr[turn];
+            arr[turn] = temp;
         }
+
     }
 
     public static void main(String[] args) {
-        int arr[] = { 5, 4, 3, 2, 1 };
-        selectionSort(arr);
-        for (int item : arr) {
-            System.out.print(item + " ");
+        int arr[] = { 5, 4, 1, 3, 2 };
+        sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
